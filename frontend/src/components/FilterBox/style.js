ba5249e76ce9fe280ae08ed55821c3067294e462
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-05 00:36:34 -0300

    style: Adiciona estilo ao componente FilterBox e Header.

---
+++ frontend/src/components/FilterBox/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import { filterBackground } from "../../constants/colors";
+
+export const FilterBoxStyled = styled.div`
+height: 35vh;
+background-color: ${filterBackground};
+`;
+
+export const FilterBoxTitle = styled.div`
+display: flex;
+text-align: center;
+color: #FFF;
+width: 550px;
+margin: 0 auto;
+padding: 30px;
+font-size: 1rem;
+font-family:  Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+`;
+
+export const FilterBoxParagraph = styled.div`
+text-align: center;
+color: #fff;
+`;
