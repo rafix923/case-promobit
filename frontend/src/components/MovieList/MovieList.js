875760cbec8a54ae55f4be13cf6acb2af763d4ce
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-06 00:21:06 -0300

    feat: Importa dentro do compoenente MovieList o componente ReactPaginate para renderizar a interface de paginação.

--- frontend/src/components/MovieList/MovieList.js
+++ frontend/src/components/MovieList/MovieList.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { fetchMovies } from "../../services/apiService";
 import MovieItem from "../MovieItem/MovieItem";
+import ReactPaginate from "react-paginate";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(5);
 
   useEffect(() => {
     const getMovies = async () => {
@@ -31,20 +33,30 @@ const MovieList = () => {
     return <div>Error: {error.message}</div>;
   }
 
+  const handlePageChange = (selected) => {
+    setCurrentPage(selected.selected + 1);
+  };
+
   return (
     <div>
       {/* Retorna a lista de filmes */}
       {movies.map((movie) => (
         <MovieItem key={movie.id} movie={movie} />
       ))}
-      {loading && <div>Loading...</div>}
-      {error && <div>Error: {error.message}</div>}
-      <button
-        type="button"
-        onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
-      >
-        Próxima Página
-      </button>
+
+      <ReactPaginate
+        pageCount={totalPages}
+        initialPage={currentPage - 1}
+        forcePage={currentPage - 1}
+        onPageChange={handlePageChange}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+        previousLabel={null}
+        nextLabel={"Próxima"}
+        breakLabel={"..."}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+      />
     </div>
   );
 };
