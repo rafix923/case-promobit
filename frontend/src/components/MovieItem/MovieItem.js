02252e9b3b75372f667988433dbfee097390c638
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-05 22:28:31 -0300

    feat: Cria variáveis e lógica dentro da função MovieList do arquivo MovieList.JS.
     O componente MovieList será responsável por buscar a lista de filmes da API utilizando a função fetchMovies, gerenciar o estado de carregamento e tratamento de erros, e renderizar a lista de filmes utilizando o componente MovieItem.

--- frontend/src/components/MovieItem/MovieItem.js
+++ frontend/src/components/MovieItem/MovieItem.js
@@ -1,14 +1,15 @@
 import React from "react";
+import { BASE_FILM_IMG } from "../../constants/urls";
 
 export default function MovieItem({ movie }) {
   return (
     <div>
       <img
-        src={`${BASE_FILM_IMG}/w500/${movie.poster_path}}`}
+        src={`${BASE_FILM_IMG}/w500/${movie.poster_path}`}
         alt={movie.title}
         style={{ width: "300px", height: "450px" }}
       />
       <h2>{movie.title}</h2>
     </div>
   );
-}
+};
