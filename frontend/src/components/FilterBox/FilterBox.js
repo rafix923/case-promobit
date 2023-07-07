b2df36530afaccc232f61ace4eca2d130798bbc9
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-07 17:06:48 -0300

    feat: Implementa a função handleFilterGenres para lidar com a lógica de filtragem dos gêneros sempre que um botão for clicado.

--- frontend/src/components/FilterBox/FilterBox.js
+++ frontend/src/components/FilterBox/FilterBox.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MovieContext } from "../../contexts/MovieContext";
 import { FilterBoxParagraph, FilterBoxStyled, FilterBoxTitle } from "./style";
+import MovieGenre from "../MovieGenre/MovieGenre";
 
 export default function FilterBox() {
+  const { genres, filters, handleFilterGenres } = useContext(MovieContext);
   return (
     <FilterBoxStyled>
       <FilterBoxTitle>
         <h1>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h1>
       </FilterBoxTitle>
       <FilterBoxParagraph>
-        <p>Filtre por:</p>
+        <h2>Filtre por:</h2>
       </FilterBoxParagraph>
-      <div>
-        <button></button>
-      </div>
+      {genres &&
+        genres.map((genre) => {
+          <MovieGenre
+            key={genre.id}
+            name={genre.name}
+            onClick={() => handleFilterGenres(genre.id)}
+            active={filters.includes(genre.id)}
+          />;
+        })}
     </FilterBoxStyled>
   );
 }
