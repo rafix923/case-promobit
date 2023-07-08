872781539e3ff82fa22c5b8014201453204c962c
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-07 22:42:59 -0300

    refactor(fix): Refatora a funcionalidade de filtragem por gênero de filmes.
    Encapsula o Router com o MovieDataProvider.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Router from "./Routes/Router";
+import { MovieDataProvider } from "./contexts/MovieContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router />
+    <MovieDataProvider>
+      <Router />
+    </MovieDataProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
