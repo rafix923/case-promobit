658923314c0d84963dc28a9834ac56103dc249a5
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-08 13:44:02 -0300

    style: Estilo do filterBoxStyled.

--- frontend/src/components/FilterBox/style.js
+++ frontend/src/components/FilterBox/style.js
@@ -4,6 +4,7 @@ import { filterBackground } from "../../constants/colors";
 export const FilterBoxStyled = styled.div`
 height: 35vh;
 background-color: ${filterBackground};
+padding-top: 46px;
 `;
 
 export const FilterBoxTitle = styled.div`
@@ -20,4 +21,5 @@ font-family:  Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', 
 export const FilterBoxParagraph = styled.div`
 text-align: center;
 color: #fff;
+padding-bottom: 23px;
 `;
