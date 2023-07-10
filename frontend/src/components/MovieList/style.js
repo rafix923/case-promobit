a7853de07d4485e37a35475be4dcbcb122c4ad77
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-10 00:28:54 -0300

    feat(style): Cria funções para a paginação por meio da seta svg e para que a palavra 'Última' desempenhe o trabalho de ir para a última página.
    Realizada estilização para que os botões, a seta de paginação e a palavra última fique alinhados no centro da página na horizontal.

--- frontend/src/components/MovieList/style.js
+++ frontend/src/components/MovieList/style.js
@@ -1,43 +1,65 @@
 import styled from "styled-components";
 
 export const CardsContainer = styled.div`
-display: flex;
-flex-direction: row;
-padding-top: 25px;
-justify-content: space-between;
-flex-wrap: wrap;
-width: 87vw;
-margin: 0 auto;
-gap: 20px;
+  display: flex;
+  flex-direction: row;
+  padding-top: 25px;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 87vw;
+  margin: 0 auto;
+  gap: 20px;
 `;
 
 export const PaginateStyled = styled.div`
- .pagination {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .pagination {
     display: flex;
     justify-content: center;
-    font-size: 16px;
-    font-family: Roboto;
+    font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
+      "Helvetica Neue", sans-serif;
     font-style: normal;
-    color: #5C16C5;
+    color: #5c16c5;
     list-style-type: none;
     font-weight: bold;
     line-height: 24px;
     margin-bottom: 200px;
+
     li {
       a {
         margin: 16px;
         cursor: pointer;
       }
     }
-  }  
+  }
+
   .active {
     font-weight: bolder;
     color: #c163e9;
+    font-size: 1rem;
   }
 
-    img{
-        width: 23px;
-        margin-top: 2px;
-        color: #5C16C5;            
-    }
+  img {
+    width: 23px;
+    margin-top: 2px;
+    color: #5c16c5;
+  }
+`;
+
+export const LastPage = styled.p`
+  color: #5c16c5;
+  text-align: end;
+  font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
+    "Helvetica Neue", sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  margin-top: -227px;
+  margin-left: 342px;
 `;
