5a9752b7ff96c8749e634916ce1b7bd8e4412149
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-09 22:05:37 -0300

    style: Estilização dos botões de paginação.
    Foi acrescentada uma imagem svg no formato de seta para a direita, como opção de ir para a próxima página.

--- frontend/src/components/MovieList/style.js
+++ frontend/src/components/MovieList/style.js
@@ -10,3 +10,34 @@ width: 87vw;
 margin: 0 auto;
 gap: 20px;
 `;
+
+export const PaginateStyled = styled.div`
+ .pagination {
+    display: flex;
+    justify-content: center;
+    font-size: 16px;
+    font-family: Roboto;
+    font-style: normal;
+    color: #5C16C5;
+    list-style-type: none;
+    font-weight: bold;
+    line-height: 24px;
+    margin-bottom: 200px;
+    li {
+      a {
+        margin: 16px;
+        cursor: pointer;
+      }
+    }
+  }  
+  .active {
+    font-weight: bolder;
+    color: #c163e9;
+  }
+
+    img{
+        width: 23px;
+        margin-top: 2px;
+        color: #5C16C5;            
+    }
+`;
