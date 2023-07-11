7e6da8d38b594018c6f2e8504e3cbfd0a41cfd24
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-11 20:29:55 -0300

    feat: Cria o arquivo BoxOverview.
    Esse arquivo será o componente renderizado pela DetailsPage.

--- frontend/src/pages/DetailsPage/DetailsPage.js
+++ frontend/src/pages/DetailsPage/DetailsPage.js
@@ -1,47 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { fetchMovieDetails } from "../../services/apiService";
+import React from "react";
 import Header from "../../components/Header/Header";
-import { BASE_FILM_IMG } from "../../constants/urls";
+import BoxOverview from "../../components/BoxOverview/BoxOverview";
 import { MainContainer } from "./style";
 
-
 export default function DetailsPage() {
-  const { id } = useParams();
-  const [movie, setMovie] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getMovieDetails = async () => {
-      try {
-        const fetchedMovie = await fetchMovieDetails(id);
-        setMovie(fetchedMovie);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
-    getMovieDetails();
-  }, [id]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
   return (
     <MainContainer>
       <Header />
-      <img
-        src={`${BASE_FILM_IMG}/w500/${movie.poster_path}`}
-        alt={movie.title}
-        style={{ width: "23rem" }}
-      />
-      <h1>{movie.title}</h1>
-      <p>{movie.overview}</p>
+      <BoxOverview />
     </MainContainer>
   );
 }
