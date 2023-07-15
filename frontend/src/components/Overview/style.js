5e0cac56d0cb649fcf563ff0461fcf4b782b7d8e
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-15 19:00:40 -0300

    Adiciona a constante Roboto ao componente Overview(style).

--- frontend/src/components/Overview/style.js
+++ frontend/src/components/Overview/style.js
@@ -1,21 +1,17 @@
 import styled from "styled-components";
 import { filterBackground } from "../../constants/colors";
+import { Roboto } from "../../constants/fonts";
 
 export const DetailsContainer = styled.div`
 width: 1900px;
 background-color: ${filterBackground};
 height: 590px;
+font-family: ${Roboto};
 `;
 
 export const ContentWrapper = styled.div`
 max-width: 1700px;
 padding: 20px;
-
-/* Estilos adicionais para dispositivos móveis */
-@media (max-width: 768px) {
-max-width: 1400px;
-padding: 10px;
-}
 `;
 
 export const ImageCard = styled.img`
@@ -30,14 +26,13 @@ box-shadow: 0 6px 26px -6px #222;
 export const DetailsPageTitle = styled.h1`
 color: #fff;
 margin: -568px 0 420px 512px;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans","Helvetica Neue", sans-serif;
-font-size: 2.2rem;
+font-family: ${Roboto};
+font-size: 2.6rem;
 `;
 
 export const MovieInfo = styled.div`
 margin:-412px 0 0 520px;
 color: #fff;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans","Helvetica Neue", sans-serif;
 font-size: 1.2rem;
 `;
 
@@ -45,7 +40,6 @@ export const ProgressbarContainer = styled.div`
 border-radius: 50%;
 width:83px;
 margin:23px 0 0 520px;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 font-size: 1.2rem;
 font-weight: 700;
 background-color: #42246d;
@@ -54,7 +48,6 @@ background-color: #42246d;
 export const LabelProgress = styled.p`
 width:130px;
 margin:-66px 0 0 620px;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 font-size: 1rem;
 font-weight: 600;
 color: #fff;
@@ -62,7 +55,6 @@ color: #fff;
 
 export const Synopsis = styled.h1`
 margin: 63px 0 0 520px;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 font-size: 1.5rem;
 font-weight: 600;
 color: #fff;
@@ -71,8 +63,7 @@ color: #fff;
 export const TextOverview = styled.p`
 margin: 12px 0 0 520px;
 padding-right: 12px;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-font-size: 1rem;
+font-size: 1.06rem;
 color: #dddddd;
 line-height: 23px;
 `;
@@ -82,15 +73,9 @@ display: flex;
 flex-direction: row;
 flex-wrap: wrap;
 margin: 32px 0 0 520px;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 color: #fff;
 line-height: 26px;
 gap: 86px;
-
-/* Estilos adicionais para dispositivos móveis */
-/* @media (max-width: 768px) {
-flex-wrap: nowrap;
-  } */
 `;
 
 export const CrewName = styled.h2`
