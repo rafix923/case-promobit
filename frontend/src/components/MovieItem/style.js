166579c386e73fa39de614b9925ed948593844ab
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-15 19:13:25 -0300

    Adiciona a constante Roboto ao componente MovieItem(style).

--- frontend/src/components/MovieItem/style.js
+++ frontend/src/components/MovieItem/style.js
@@ -1,18 +1,19 @@
 import styled from "styled-components";
+import { Roboto } from "../../constants/fonts";
 
 export const MovieTitle = styled.h2`
 text-align: left;
 width: 180px;
 font-size: 1.1rem;
 font-weight: 600;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+font-family: ${Roboto};
 pointer-events: none;
 `;
 
 export const MovieRealese = styled.p`
 color: #646464;
 font-size: 14px;
-font-family: Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+font-family: ${Roboto};
 font-style: normal;
 font-weight: 700;
 line-height: 3vh;
