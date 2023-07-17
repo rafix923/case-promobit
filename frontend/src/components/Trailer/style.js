932a901a384c5495d004951e5bf38a70f9307839
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-07-17 02:01:57 -0300

    style: Responsividade para a seção trailer do filme.
    Tela mobile.

--- frontend/src/components/Trailer/style.js
+++ frontend/src/components/Trailer/style.js
@@ -5,8 +5,19 @@ export const Title = styled.h1`
 margin: 35px 0 21px 120px;
 font-family: ${Roboto};
 font-size: 2.2rem;
+
+@media((min-width:320px) and (max-width: 480px)){
+   margin: 46px 0 16px 32px;
+   width: 326px;
+   font-size: 1.9rem;
+}
 `;
 
 export const Video = styled.div`
 margin: 35px 0 21px 120px;
+
+
+@media((min-width:320px) and (max-width: 480px)){
+   margin: 0 0 0 32px;
+}
 `;
